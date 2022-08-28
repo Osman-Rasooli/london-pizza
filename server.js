d18a645cd4e9418d99802a590362a038c415e10a
Mohammad Osman Rasooli <thunder1973bolt@gmail.com>
2022-08-29 02:04:42 +0430

    Added Login and Register pages

--- server.js
+++ server.js
@@ -23,6 +23,14 @@ app.get('/cart', (req, res) => {
     res.render('customers/cart');
 })
 
+app.get('/login', (req, res) => {
+    res.render('auth/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register')
+})
+
 app.listen(PORT, () => {
     console.log('Listening on port 5000!');
 })
