ea5d0ab7079c25b19e1c1a88e195b8abddab148c
Osman Rasooli <mohammad.osmanrasooli1973@gmail.com>
2022-08-30 02:53:06 +0430

    Connected to MongoDB

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
@@ -6,6 +8,17 @@ const path = require('path');
 const app = express();
 
 const PORT = 5000;
+
+// Database Connectivity
+const uri = "mongodb+srv://admin:&$Lx3ChJQ*gbPq^@mern.tvekf.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("pizza").collection("menu");
+  // perform actions on the collection object
+
+  console.log('database connected!');
+  // client.close();
+});
 
 app.use(express.static('public'));
 
